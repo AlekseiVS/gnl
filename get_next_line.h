64fb5d0c615f3d5e9dc0f77d6a10b620a312cb4c
Oleksii SOKOLIUK <osokoliu@e1r11p3.unit.ua>
2018-02-09 16:27:02 +0200

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osokoliu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/05 11:24:49 by osokoliu          #+#    #+#             */
+/*   Updated: 2018/01/05 11:33:24 by osokoliu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
+
+#endif
